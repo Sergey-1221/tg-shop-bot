a86baaa4b9c1edb7c8efa4d08ccaf1eed5798d10
aelksey <alekseyvlad1miroff@yandex.ru>
2023-10-21 18:00:54 +0700

    test

--- models.py
+++ models.py
@@ -6,5 +6,21 @@ class Product(Base):
     name = Column(String(100))
     price = Column(Float)
 
+'''
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer)
+    balance = Column(Float)
+    is_admin = Column(Boolean)
+
+class Adminpasswords(Base):
+    __tablename__ = 'passwords'
+    id = Column(Integer)
+    password = Column(String)
+'''
+
+
+
+
 if __name__ == "__main__":
     Base.metadata.create_all()
