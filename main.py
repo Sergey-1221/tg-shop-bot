8a2bdc94bc53f6be5c6598ee5dc006eb62b97050
Sergey <59055584+Sergey-1221@users.noreply.github.com>
2023-10-22 01:35:28 +0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
+from telebot import types
 from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import Product,Users
 
@@ -14,69 +14,101 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-'''
-#Тест веь-приложения раскоментить чтоб заработало
-@bot.message_handler(commands=['start'])
-def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
-   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
-   webAppTest = types.WebAppInfo("https://telegram.mihailgok.ru") #создаем webappinfo - формат хранения url
-   one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
-   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
-    return keyboard #возвращаем клавиатуру
-'''
+
+
 
 # Обработчик команды /start
-#Проверка на админа
 
-class AdminChecker:
-    is_admin = False
-                   
-adminka = AdminChecker()    
+def check_user(user_id):
+    user = session.get(Users, user_id)
+    if user == None:
+        user = Users(tg_id=user_id)
+        session.add(user)
+        session.commit()
+    is_admin = (user.role == "admin")
+
+    return {
+        "object": user,
+        "admin": is_admin
+        }
+
+
+def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
+   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
+   webAppTest = types.WebAppInfo("https://192.168.26.27:50100") #создаем webappinfo - формат хранения url
+   one_butt = types.KeyboardButton(text="Товары", web_app=webAppTest) #создаем кнопку типа webapp
+   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
+
+   return keyboard #возвращаем клавиатуру
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
-    bot.reply_to(message, 'Добро пожаловать в наш магазин')
-    users = session.query(Users).all()
-    for user in users:
-        if user.tg_id == message.from_user.id:
-            bot.send_message(chat_id,'Вы сотрудник компании')
-            if user.role == 'admin':
-                adminka.is_admin = True
-                bot.send_message(chat_id,'Вы админ, можете удалять добавлять и т.д товары')
+    user = check_user(message.from_user.id)
+    #bot.reply_to(message, 'Добро пожаловать в наш магазин')
+    bot.send_message(chat_id, 'Добро пожаловать в наш магазин', reply_markup=webAppKeyboard())
+    bot.send_message(chat_id,'/me Информация обо мне \n/help список всех команд')
+            
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    chat_id = message.chat.id
+    user = check_user(message.from_user.id)
+
+    if user["admin"]:
+        bot.send_message(chat_id, 'Команды администратора:\n /add Добавить товар')
 
     
 # Обработчик команды /add
 @bot.message_handler(commands=['add'])
 def add_product(message):
     chat_id = message.chat.id
-    if adminka.is_admin == True:
-        
+    user = check_user(message.from_user.id)
+    if user["admin"]:
         msg = bot.send_message(chat_id, 'Введите название продукта:')
-        bot.register_next_step_handler(msg, process_name_step)
-    else:
-        bot.send_message(chat_id, 'Вы не админ')    
+        bot.register_next_step_handler(msg, process_name_step) 
+        
+        
 
 def process_name_step(message):
     chat_id = message.chat.id
     name = message.text
     msg = bot.send_message(chat_id, 'Введите цену продукта:')
     bot.register_next_step_handler(msg, process_price_step, name)
-    bot.send_message(chat_id, 'Вы не админ')
 
 
 def process_price_step(message, name):
     chat_id = message.chat.id
-    price = float(message.text)
+    try:
+        price = float(message.text)
+        msg = bot.send_message(chat_id, 'Загрузите изображение:')
+        bot.register_next_step_handler(msg, process_image_step, name, price) 
 
-    # Добавляем продукт в базу данных
-    product = Product(name=name, price=price)
-    session.add(product)
-    session.commit()
+    except ValueError:
+        msg = bot.send_message(chat_id, 'Введите коректоне число (Например 3.14):')
+        bot.register_next_step_handler(msg, process_price_step, name)
 
-    bot.send_message(chat_id, 'Продукт успешно добавлен!')
-    
+def process_image_step(message, name, price):   
+    chat_id = message.chat.id
+    if message.photo != None:
+        file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        name_image = message.photo[1].file_id+".jpg"
+        src = 'image/' + message.photo[1].file_id+".jpg"
+        
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Фото добавлено")
+        product = Product(name=name, price=price, image=name_image)
+        session.add(product)
+        session.commit()
+        bot.send_message(chat_id, 'Продукт успешно добавлен!')
+    else:
+        msg = bot.send_message(chat_id, 'Фото не найдено.')
+        bot.register_next_step_handler(msg, process_image_step, name, price) 
+ 
+
 
 # Обработчик команды /list
 @bot.message_handler(commands=['list'])
--- models.py
+++ models.py
@@ -16,34 +16,27 @@ class Product(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     price = Column(Float)
-    descriptions = Column(String(500))
-
-    basket = relationship("Basket", backref="basket")
+    #descriptions = Column(String(500))
+    image = Column(String(255))
+    basket = relationship("Basket", backref="products")
 
 
 class Users(Base):
     __tablename__ = 'users'
     tg_id = Column(BigInteger, primary_key=True, index=True)#ID пользоваетеля из телеграма 
-    role = Column(String(100), nullable=False)#Роль admin/user 
-
-    basket = relationship("Basket", backref="basket")
+    role = Column(String(100), nullable=False, default="user")#Роль admin/user 
+    balance = Column(Float, default=0.0)
+    basket = relationship("Basket", backref="users")
 
 class Basket(Base):
     __tablename__ = 'basket'
     id = Column(Integer, primary_key=True)
-    users_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship("Users", backref="users")
-
+    users_id = Column(BigInteger, ForeignKey('users.tg_id'))
     product_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship("Product", backref="products")
-
+    status = Column(String(100),  default="0") 
 
 
     
-
-
-
-
 if __name__ == "__main__":
     engine = create_engine('sqlite:///store.db')
     Base.metadata.create_all(engine)
