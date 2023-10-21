78c6cdc7ca5fe06580ecc8cd233a7713d9b6a8d3
aelksey <alekseyvlad1miroff@yandex.ru>
2023-10-21 17:37:45 +0700

    webapptest

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ class Product(Base):
 # Создание таблицы продуктов
 #Base.metadata.create_all()
 
+'''
+#Тест веь-приложения раскоментить чтоб заработало
+@bot.message_handler(commands=['start'])
+def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
+   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
+   webAppTest = types.WebAppInfo("https://telegram.mihailgok.ru") #создаем webappinfo - формат хранения url
+   one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
+   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
+    return keyboard #возвращаем клавиатуру
+'''
+
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
