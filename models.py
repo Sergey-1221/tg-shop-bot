5cbb6af683121e1ee46a918b1c29b76a0e10c933
Sergey <59055584+Sergey-1221@users.noreply.github.com>
2023-10-21 13:38:56 +0300

    Update

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
-Base = declarative_base(bind=engine)
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 class Product(Base):
     __tablename__ = 'products'
@@ -6,5 +10,12 @@ class Product(Base):
     name = Column(String(100))
     price = Column(Float)
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    #tg_id = #ID пользоваетеля из телеграма 
+
+
 if __name__ == "__main__":
-    Base.metadata.create_all()
+    engine = create_engine('sqlite:///store.db')
+    Base.metadata.create_all(engine)
